5b1e562d6a7cce56ecf8966cc0bd69cbf1a467b1
dhs <dhs@DESKTOP-DHSHOME>
2023-09-15 03:12:47 +0900

    문의사항

--- adventour/WebContent/mypage/mpg_questions.js
+++ adventour/WebContent/mypage/mpg_questions.js
@@ -1,6 +1,14 @@
 /*mpg_questions.js*/
 
 $(document).ready(function() {
-//	헤더랑 푸터 담은 mpg_nvbox.html 로드
+	//	헤더랑 푸터 담은 mpg_nvbox.html 로드
     $("#mpg_nvbox").load("mpg_nvbox.html");
+    
+    //첫번 째 탭기능 변수 선언
+    var frstLtab1 = $('.frstLTab p').eq(0);
+    var frstLtab2 = $('.frstLTab p').eq(1);
+    var frstLtab3 = $('.frstLTab p').eq(2);
+    var secLCtg = $('.sec_lne_ctgry').eq(0);//자주묻는 질문 클릭하면 보이는 카테고리.
+    
+    //두 번쨰 탭 기능 변수 선언    
 });
