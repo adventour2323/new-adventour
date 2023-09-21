93fccc9f79850197666c4ca3be55e4d886aaf841
Administrator <Administrator@User -2023QVKLK>
2023-09-21 13:22:02 +0900

    마이페이지 css 및 js 수정.

--- adventour/WebContent/js/mpg_questions.js
+++ adventour/WebContent/js/mpg_questions.js
@@ -6,37 +6,39 @@ $(document).ready(function() {
     var sndBtn = document.getElementsByName("sndBtn")[0];//문의하기 보내기 버튼
     
     //첫번 째 탭기능 변수 선언
-    var frstLtab1 = $('.frstLTab p').eq(0);
-    var frstLtab2 = $('.frstLTab p').eq(1);
-    var frstLtab3 = $('.frstLTab p').eq(2);
+    var frstLtab1 = $('.frstLTab p').eq(0);/*문의 내역*/
+    var frstLtab2 = $('.frstLTab p').eq(1);/*자주 묻는 질문*/
+    var frstLtab3 = $('.frstLTab p').eq(2);/*문의하기*/
     var secLCtg1 = $('.my_qna').eq(0);//문의 내역 클릭하면 보이는 카테고리.
     var secLCtg2 = $('.sec_lne_ctgry').eq(0);//자주묻는 질문 클릭하면 보이는 카테고리.
     var secLCtg3 = $('.write').eq(0);//문의하기 클릭하면 보이는 카테고리.
     
+    
 //    첫번 째 탭기능 디폴트
     secLCtg1.hide();
     secLCtg3.hide();
-    
+
+    /*문의 내역 탭 클릭 시*/
     frstLtab1.click(function () {       
        loadMyQna();
-       
        secLCtg1.show();
        secLCtg2.hide();
        secLCtg3.hide();
     });
+    /*자주묻는 질문 탭 클릭 시*/
     frstLtab2.click(function () {       
        secLCtg2.show();
        secLCtg1.hide();
        secLCtg3.hide();
     });
+    /*문의 하기 탭 클릭 시*/
     frstLtab3.click(function () {       
        secLCtg3.show();
        secLCtg1.hide();
        secLCtg2.hide();
     });
     
-  //두번 째 탭기능 변수 선언   
-    var sec_lne_ctgry = $('sec_lne_ctgry');  
+  //두번 째 탭기능 변수 선언 
     var secLTab_t = $('.secLTab').eq(0);
     var secLTab_b = $('.secLTab').eq(1);
     var secLTab_h = $('.secLTab').eq(2);
@@ -48,8 +50,8 @@ $(document).ready(function() {
     var b_QnA_li = $('.b_qna_all').eq(0);
     var h_QnA_li = $('.h_qna_all').eq(0);
     var s_QnA_li = $('.s_qna_all').eq(0);
-    var o_QnA_liP = $('.oP_qna_all').eq(0);
-    var c_QnA_lis = $('.cs_qna_all').eq(0);
+    var oP_QnA_li = $('.oP_qna_all').eq(0);
+    var cs_QnA_li = $('.cs_qna_all').eq(0);
   
     /*투어 탭 클릭했을 경우 투어 관련 문의 리스트만 나옴.*/
     secLTab_t.click(function () {       
@@ -144,38 +146,8 @@ function QInfoSendF() {
 		}
 	});
 }
-//function loadMyQna(){
-//	
-//	$.ajax({
-//		url: 'mpg_questions_select.jsp',
-//		type: 'POST',
-//		dataType: 'text',
-//		data: "m_id=qkqh4848",
-//		success: function(data) {
-//			try{
-//				var myqnaCkB = $('#myqnaCkB');
-//				myqnaCkB.empty();
-//				var result = JSON.parse(data.trim());
-//				for(var i in result){
-////					myqnaCkB.append($('<option>', {
-////						value: row.engNm,
-////						text: row.korNm
-////					}));
-//					myqnaCkB.append($('<label><input type="checkbox" title="" value="" name="">'+result[i].q_title+'</label><p>답변완료</p>'));
-//				}
-//			}catch(error){
-////				error
-//			}
-//		},
-//		error: function(error) {
-//			console.error('Request error:', error);
-//		}
-//	});
-//	
-//}
 
-
-
+/*문의 내역 클릭할 경우 문의한 내역들 로드되는 펑션*/
 function loadMyQna(){
 	
 	$.ajax({
@@ -186,28 +158,39 @@ function loadMyQna(){
 		success: function(data) {
 			try{
 				var myqnaCkB = $('#myqnaCkB');
-				var qnaDtB = $('.qna_dtail_box');
+				var divStr_Tt= $('#db_Pul_Title');
+				var divStr_Cnt= $('#db_Pul_cntt');
+				var divStr_Tm= $('#write_time');
 				myqnaCkB.empty();
-				qnaDtB.empty();
+//				divStr_Tt.empty();
+//				divStr_Cnt.empty();
+//				divStr_Tm.empty();
+				
 				var result = JSON.parse(data.trim());
 				for(var i in result){
 //					myqnaCkB.append($('<option>', {
 //						value: row.engNm,
 //						text: row.korNm
 //					}));
-					myqnaCkB.append($('<label><input type="checkbox" title="" value="" name="">'+result[i].q_title+'</label><p>답변완료</p>'));
+					myqnaCkB.append($('<span id="myqnaCkB_'+result[i].q_title+'">'+result[i].q_title+'<span>'));
 //					var myQ = myqnaCkB.children('label').eq(i);
-					var myQ = myqnaCkB.find('input').eq(i);
-					/*var qtitle= $("#db_Pul_Title");*/
+					var myQ = myqnaCkB.find('span').eq(i);
 					
 //					var로하면 안 댐
 //					var divStr = '<div class="qna_dtail_box"><div class="qna_dtail_box_band"><div class="myquestion"><h4 class=""><span id="db_Pul_Title">'+result[i].q_title+'</span></h4><h4 class="">'+result[i].q_cntt+'</h4><h6>작성시간 : '+result[i].q_date+'</h6></div>'; 
-					let divStr = '<div class="qna_dtail_box"><div class="qna_dtail_box_band"><div class="myquestion"><h4 class=""><span id="db_Pul_Title_'+result[i].q_title+'">'+result[i].q_title+'</span></h4><h4 class="">'+result[i].q_cntt+'</h4><h6>작성시간 : '+result[i].q_date+'</h6></div>'; 
+					let divStr_t= '<span id="db_Pul_Title_'+result[i].q_title+'">'+result[i].q_title+'</span>';
+					let divStr_c= '<span id="db_Pul_cntt_'+result[i].q_cntt+'">'+result[i].q_cntt+'</span>';
+					let divStr_tm= '<span id="write_time_'+result[i].q_date+'">작성시간 : '+result[i].q_date+'</span>'; 
+					
 					if(myQ){
 						myQ.click(function(){
-							qnaDtB.empty();
+							divStr_Tt.empty();
+							divStr_Cnt.empty();
+							divStr_Tm.empty();
 //							qnaDtB.append($('<div class="qna_dtail_box"><div class="qna_dtail_box_band"><div class="myquestion"><h4 class=""><span id="db_Pul_Title">'+result[i].q_title+'</span></h4><h4 class="">'+result[i].q_cntt+'</h4><h6>작성시간 : '+result[i].q_date+'</h6></div>'));
-							qnaDtB.append($(divStr));
+							divStr_Tt.append($(divStr_t));
+							divStr_Cnt.append($(divStr_c));
+							divStr_Tm.append($(divStr_tm));
 						})
 					}
 				}
